e2049742f3388b235d6b0d8648393d65c3a8c2bc
Alexcool3 <Alex33@live.dk>
2019-03-22 21:20:18 +0100

    Movement Script + Movement Scene
    - Movement Script added - Basic Movement
    - Movement Scene added

---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 0f;
+      
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode up;
+    public KeyCode down;
+    public GameObject gameObejct;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObejct.GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left))
+        {
+            gameObejct.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0));
+            Debug.Log("A: Pressed");
+        } else if (Input.GetKey(right)){
+            gameObejct.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0));
+            Debug.Log("D: Pressed");
+        } else if (Input.GetKey(up)){
+            gameObejct.transform.Translate(new Vector2(0, moveSpeed * Time.deltaTime));
+            Debug.Log("W: Pressed");
+        } else if (Input.GetKey(down)) {
+            gameObejct.transform.Translate(new Vector2(0, -moveSpeed * Time.deltaTime));
+            Debug.Log("S: Pressed");
+        }
+    }
+}
