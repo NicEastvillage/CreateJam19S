9f74975cc1f3820d5366d6023d6528128f742cda
Alexcool3 <Alex33@live.dk>
2019-03-22 22:14:33 +0100

    Movement update

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player Two")
+        {
+            // Do stuff
+            onCollision();
+            Debug.Log(other.name);
+        }
+        else if (other.name == "Player 1")
+        {
+            onCollision();
+            Debug.Log(other.name);
+        }
+
+    }
+
+    private void onCollision()
+    {
+        // Do collision stuff
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 0f;
-      
+    //public float distance = 0f;  
     public KeyCode left;
     public KeyCode right;
     public KeyCode up;
@@ -15,25 +15,38 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObejct.GetComponent<GameObject>();
+        gameObejct.GetComponent<GameObject>();   
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement();
+    }
+
+    void movement()
+    {
         if (Input.GetKey(left))
         {
             gameObejct.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0));
             Debug.Log("A: Pressed");
-        } else if (Input.GetKey(right)){
+        }
+        else if (Input.GetKey(right))
+        {
             gameObejct.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0));
             Debug.Log("D: Pressed");
-        } else if (Input.GetKey(up)){
+        }
+        else if (Input.GetKey(up))
+        {
             gameObejct.transform.Translate(new Vector2(0, moveSpeed * Time.deltaTime));
             Debug.Log("W: Pressed");
-        } else if (Input.GetKey(down)) {
+        }
+        else if (Input.GetKey(down))
+        {
             gameObejct.transform.Translate(new Vector2(0, -moveSpeed * Time.deltaTime));
             Debug.Log("S: Pressed");
         }
     }
+
+    
 }
