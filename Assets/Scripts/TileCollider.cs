5cec4835f662a3ab80fccab2a648e58a6a63fe3b
Adamsen5482 <tadams17@student.aau.dk>
2019-03-23 12:08:31 +0100

    fancy stuff

---
+++ Assets/Scripts/TileCollider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCollider : MonoBehaviour
+{   bool collided;
+    Color Startcolor;
+    int changecolorbacktime=2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer colordims = GetComponent<SpriteRenderer>();
+        Startcolor = colordims.color;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        StartCoroutine(OnTriggerEnterresetcolor(collision));
+        
+    }
+   
+
+    IEnumerator OnTriggerEnterresetcolor(Collider2D collider)
+    {
+        
+        yield return new WaitForSeconds(changecolorbacktime);
+       
+           SpriteRenderer color = GetComponent<SpriteRenderer>();
+            color.color = Startcolor;
+        
+    }
+
+   
+}
---
+++ Assets/timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class timer : MonoBehaviour
+{
+    
+    
+    public float totalTime; 
+
+
+    private void FixedUpdate()
+    {
+        totalTime -= Time.deltaTime;
+        UpdateLevelTimer(totalTime);
+    }
+
+    public void UpdateLevelTimer(float totalSeconds)
+    {
+        int minutes = Mathf.FloorToInt(totalSeconds / 60f);
+        int seconds = Mathf.RoundToInt(totalSeconds % 60f);
+
+        string formatedSeconds = seconds.ToString();
+
+        if (seconds == 60)
+        {
+            seconds = 0;
+            minutes += 1;
+        }
+
+        GetComponent<Text>().text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
