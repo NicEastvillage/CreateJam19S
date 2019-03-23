906747931fbf2b3a09fbfbd6af1a6f2c0e4c75a3
Morten Bach <36537080+mbja17@users.noreply.github.com>
2019-03-23 23:45:48 +0100

    SPLASH BITCH

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+	public GameObject splashPresent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Invoke("UpdateSplash", 2);
+    }
+
+    
+	void UpdateSplash()
+	{
+		splashPresent.SetActive(true);
+		gameObject.SetActive(false);
+		Invoke("LaunchMenu", 2);
+	}
+
+	void LaunchMenu()
+	{
+		SceneManager.LoadScene("StartMenu");
+	}
+
+}
