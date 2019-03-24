5ecb13c5fbf05af3e2373d3950624cd60780df0c
NicEastvillage <nicoesterby@gmail.com>
2019-03-24 10:33:44 +0100

    Added laser explosions.

--- Assets/Scripts/TrailDanger.cs
+++ Assets/Scripts/TrailDanger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrailDanger : MonoBehaviour
 {
     public string trailTag = "Trail";
+    public GameObject explosionPrefab;
     
     void OnTriggerExit2D(Collider2D collider)
     {
